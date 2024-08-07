8ea8ba2d968fb4a039d4b365362cc82741209093
Surou <cppfuns@gmail.com>
2024-08-07 12:22:41 +0800

    update# code

--- install.sh
+++ install.sh
@@ -21,33 +21,36 @@ mkdir -p ./orange-external-node/geth/logs
 mkdir -p ./orange-external-node/geth/config
 mkdir -p ./orange-external-node/op-node/logs
 mkdir -p ./orange-external-node/op-node/config
+cd orange-external-node/
 
-echo -n 0x$(openssl rand -hex 32 | tr -d "\n") > ./orange-external-node/geth/config/jwtsecret
-cp ./orange-external-node/geth/config/jwtsecret ./orange-external-node/op-node/config/jwtsecret
+echo -n 0x$(openssl rand -hex 32 | tr -d "\n") > ./geth/config/jwtsecret
+cp ./orange-external-node/geth/config/jwtsecret ./op-node/config/jwtsecret
+
+wget http://18.167.109.180:8866/orange-external-node/orange-external-node/geth/geth -O ./geth/geth
+wget http://18.167.109.180:8866/orange-external-node/orange-external-node/op-node/op-node -O ./op-node/op-node
+chmod +x ./geth/geth
+chmod +x ./op-node/op-node
 
 echo "Select the network you want to join \n"
 echo -e " 1.Orange Mainnet\n 2.Orange Testnet\n"
 read -p "Enter index: " netType;
 if [ "$netType" == "1" ];then
-    wget http://18.167.109.180:8866/orange-external-node/geth/mainnet/run.sh -O ./orange-external-node/geth/run.sh
-    wget http://18.167.109.180:8866/orange-external-node/geth/mainnet/config/config.toml -O ./orange-external-node/geth/config/config.toml
-    wget http://18.167.109.180:8866/orange-external-node/geth/mainnet/config/genesis.json -O ./orange-external-node/geth/config/genesis.json
+    wget http://18.167.109.180:8866/orange-external-node/orange-external-node/geth/mainnet/run.sh -O ./geth/run.sh
+    wget http://18.167.109.180:8866/orange-external-node/orange-external-node/geth/mainnet/config/config.toml -O ./geth/config/config.toml
+    wget http://18.167.109.180:8866/orange-external-node/orange-external-node/geth/mainnet/config/genesis.json -O ./geth/config/genesis.json
+    wget http://18.167.109.180:9002/snapshot.tar.gz
+    tar -xzvf snapshot.tar.gz
 elif [ "$netType" == "2" ]; then
-    wget http://18.167.109.180:8866/orange-external-node/geth/testnet/run.sh -O ./orange-external-node/geth/run.sh
-    wget http://18.167.109.180:8866/orange-external-node/geth/testnet/config/config.toml -O ./orange-external-node/geth/config/config.toml
-    wget http://18.167.109.180:8866/orange-external-node/geth/testnet/config/genesis.json -O ./orange-external-node/geth/config/genesis.json
+    wget http://18.167.109.180:8866/orange-external-node/orange-external-node/geth/testnet/run.sh -O ./geth/run.sh
+    wget http://18.167.109.180:8866/orange-external-node/orange-external-node/geth/testnet/config/config.toml -O ./geth/config/config.toml
+    wget http://18.167.109.180:8866/orange-external-node/orange-external-node/geth/testnet/config/genesis.json -O ./geth/config/genesis.json
+    wget http://18.167.109.180:9002/snapshot-testnet.tar.gz
+    tar -xzvf snapshot-testnet.tar.gz
 else
     echo "The current network does not support"
     exit 1;
 fi
-
-cd orange-external-node/
-wget http://18.167.109.180:9002/snapshot.tar.gz
-tar -xzvf snapshot.tar.gz
 mv data_bak geth/data
-
-
-
 chmod +x ./geth/run.sh
 chmod +x ./geth/geth
 chmod +x ./op-node/run.sh
